442dc3bf2793001e115c3028a1e3352ebfe945e3
cabanga <manucabanga@gmail.com>
2023-12-11 20:28:42 +0100

    chore: Teste Main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    baseURL: 'https://api.kambandu.com/',
+    baseURL: 'https://qas.kambandu.com/',
     baseVersion: 'v1',
-    fullBaseUrl: `https://api.kambandu.com/api/v1`,
+    fullBaseUrl: `https://qas.kambandu.com/api/v1`,
     invoiceServe: {
         baseUrl: 'https://print.kambandu.com/api/v1/document',
         doc_uuid: 'f20d588d-acff-4278-9887-c8eeb93903e1',
