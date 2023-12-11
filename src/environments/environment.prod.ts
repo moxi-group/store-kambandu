600fcdf42f437f102907ad69bcc9a62007a56375
cabanga <manucabanga@gmail.com>
2023-12-11 13:08:35 +0100

    this commit Config  Enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
     baseVersion: 'v1',
     fullBaseUrl: `https://api.kambandu.com/api/v1`,
     invoiceServe: {
-        baseUrl: 'http://139.162.231.221:3000/api/v1/document',
-        doc_uuid: 'bf815926-c74e-4f52-beec-5717239ed079',
+        baseUrl: 'https://print.kambandu.com/api/v1/document',
+        doc_uuid: 'f20d588d-acff-4278-9887-c8eeb93903e1',
         output: 'generate?output_format=pdf'
     }
 };
