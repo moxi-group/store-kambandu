cc9f395c5ad24c673e5ec8e0534ee5c2a0e889bf
cabanga <manucabanga@gmail.com>
2024-01-19 09:22:23 +0100

    chore: RELEASE 2.0.1, 18-01-2024

--- src/app/views/layout/footer/footer.component.ts
+++ src/app/views/layout/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  version_app: string = '2.0.1, 18-01-2024'
+  version_app: string = '2.0.3, 19-01-2024'
   constructor() { }
 
   ngOnInit(): void {
