c423559f0c4b95d702a35655414c2c69776fc3b4
cabanga <manucabanga@gmail.com>
2024-02-07 19:15:28 +0100

    chore: Config Report Invoices

---
+++ src/app/views/reports/reports.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportsService {
+    constructor(private _http_client: HttpClient) { }
+
+    private token = sessionStorage.getItem('sessionToken')
+    private companyToken = sessionStorage.getItem('CURRENT_COMPANY')
+
+
+
+    public headers = new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*')
+    .set('Authorization', `Bearer ${this.token}`)
+    .set('header-company-uuid', `${this.companyToken}`)
+
+
+    reports_billigs(filter: any) {
+        return this._http_client.get<any>(
+            `${environment.fullBaseUrl}/reports/all-sales`, 
+            {
+                params: filter,
+                headers: this.headers
+            }
+        )
+    }
+
+
+}
