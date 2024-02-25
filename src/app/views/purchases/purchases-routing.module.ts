87fa2af0d80c4ddc42d6dfa62fcea9c10e303126
cabanga <manucabanga@gmail.com>
2024-02-25 16:31:43 +0100

    this commit config form to add order

--- src/app/views/purchases/purchases-routing.module.ts
+++ src/app/views/purchases/purchases-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProvidersComponent } from './providers/providers.component';
 import { OrdersComponent } from './orders/orders.component';
+import { CreateOrEditOrderComponent } from './orders/create-or-edit-order/create-or-edit-order.component';
 
 const routes: Routes = [
     {
@@ -9,18 +10,16 @@ const routes: Routes = [
         children: [
             {
                 path: 'providers', 
-                component: ProvidersComponent,
-                children: [
-                    {
-                        path: 'create-provider', 
-                        component: ProvidersComponent
-                    }
-                ]
+                component: ProvidersComponent
             },
             {
                 path: 'orders', 
                 component: OrdersComponent
-            }
+            },
+            {
+                path: 'create-order', 
+                component: CreateOrEditOrderComponent
+            },
         ]
     },
 ];
