0370b789c9d318dd358a65801e8e3aaa40f0eba0
cabanga <manucabanga@gmail.com>
2024-04-24 14:51:07 +0100

    this commit Config payments pages

--- src/app/views/layout/footer/footer.component.ts
+++ src/app/views/layout/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  version_app: string = '3.0.3, 23/04/2024'
+  version_app: string = '3.0.4, 24/04/2024'
   constructor() { }
 
   ngOnInit(): void {
